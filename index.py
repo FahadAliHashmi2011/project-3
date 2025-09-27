103100617c3a8aa36b6478d62f92801f357ceb37
FahadAliHashmi2011 <faiezali2006@gmail>
2025-09-27 15:06:02 +0100

    s

--- index.py
+++ index.py
@@ -1,3 +1,17 @@
 num=int(input("enter a number: "))
 import math
 print (math.sqrt(num))
+
+
+# summer time
+weather =(input("what is the weather today ?(choose from warm, cold, rainy or snowing) :"))
+if weather == "warm" :
+ print (" you can go out side without a coat")
+elif weather == "cold" :
+ print(" you should take a coat and proper shoes however not necessary")
+elif weather == "rainy" :
+ print ("You will need a coat ,proper shoes, and an umbrella")
+elif weather == "snowing" :
+ print("u will need to take a coat, thick trousers, good socks, shoes, gloves and a hat")
+else :
+ print(" u did not choose one of the options")
